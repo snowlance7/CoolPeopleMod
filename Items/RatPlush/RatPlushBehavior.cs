1489814f894c113eb52b83d8c04eb5dac9ecf013
snowlance7 <znowlance@gmail.com>
2025-02-01 19:57:43 -0600

    ready for small release

--- Items/RatPlush/RatPlushBehavior.cs
+++ Items/RatPlush/RatPlushBehavior.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using GameNetcodeStuff;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.AI;
+using static CoolPeopleMod.Plugin;
 
 namespace CoolPeopleMod.Items.RatPlush
 {
@@ -16,8 +19,72 @@ namespace CoolPeopleMod.Items.RatPlush
         public AudioSource ItemAudio;
         public AudioClip[] BirthdaySFX;
         public Animator ItemAnimator;
+        public GameObject RatPrefab;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
+        RatScript? currentRat;
+        Vector3 mainEntrancePosition;
 
+        public override void Start()
+        {
+            base.Start();
+            mainEntrancePosition = RoundManager.FindMainEntrancePosition(true);
+        }
+
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+
+            if (buttonDown)
+            {
+                RoundManager.PlayRandomClip(ItemAudio, BirthdaySFX);
+                ItemAnimator.SetTrigger("squeeze");
+
+                if (IsSlayerInGame()) { return; }
+
+                if (playerHeldBy.playerSteamId == RatSteamID)
+                {
+                    TrySpawnRatOnServer();
+                }
+            }
+        }
+
+        void TrySpawnRatOnServer()
+        {
+            if (!IsServerOrHost) { return; }
+            if (!CalculatePath(playerHeldBy.transform.position, mainEntrancePosition)) { return; }
+            if (currentRat != null)
+            {
+                currentRat.DisappearClientRpc();
+            }
+
+            float forwardOffset = 1f;
+            Vector3 positionInFrontOfPlayer = playerHeldBy.transform.position + playerHeldBy.transform.forward * forwardOffset;
+
+            currentRat = GameObject.Instantiate(RatPrefab, positionInFrontOfPlayer, playerHeldBy.transform.rotation).GetComponent<RatScript>();
+            currentRat.NetworkObject.Spawn(true);
+        }
+
+        bool IsSlayerInGame()
+        {
+            foreach (var player in StartOfRound.Instance.allPlayerScripts)
+            {
+                if (player.playerSteamId == SlayerSteamID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CalculatePath(Vector3 fromPos, Vector3 toPos)
+        {
+            Vector3 from = RoundManager.Instance.GetNavMeshPosition(fromPos, RoundManager.Instance.navHit, 1.75f);
+            Vector3 to = RoundManager.Instance.GetNavMeshPosition(toPos, RoundManager.Instance.navHit, 1.75f);
+
+            NavMeshPath path = new();
+            return NavMesh.CalculatePath(from, to, -1, path) && Vector3.Distance(path.corners[path.corners.Length - 1], RoundManager.Instance.GetNavMeshPosition(to, RoundManager.Instance.navHit, 2.7f)) <= 1.55f; // TODO: Test this
+        }
     }
 }
--- Items/RatPlush/RatScript.cs
+++ Items/RatPlush/RatScript.cs
@@ -7,14 +7,80 @@ using UnityEngine.AI;
 
 namespace CoolPeopleMod.Items.RatPlush
 {
-    internal class RatScript : NetworkBehaviour
+    internal class RatScript : NetworkBehaviour // TODO: Finish this
     {
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public AudioSource RatAudio;
         public AudioClip BirthdayMixtapeSFX;
         public NavMeshAgent agent;
+        public float AIIntervalTime;
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
+        float timeSinceAIInterval;
+        Vector3 mainEntrancePosition;
+        NavMeshPath path1;
+        bool moveTowardsDestination;
+        Vector3 destination;
 
+        public void Start()
+        {
+            mainEntrancePosition = RoundManager.FindMainEntrancePosition(true);
+
+            
+        }
+
+        public void Update()
+        {
+            timeSinceAIInterval += Time.deltaTime;
+            if (timeSinceAIInterval > AIIntervalTime)
+            {
+                DoAIInterval();
+                timeSinceAIInterval = 0;
+            }
+        }
+
+        void DoAIInterval()
+        {
+            if (moveTowardsDestination)
+            {
+                agent.SetDestination(destination);
+            }
+
+            if (!SetDestinationToPosition(mainEntrancePosition, true) || Vector3.Distance(transform.position, mainEntrancePosition) < 1f)
+            {
+                Disappear();
+            }
+        }
+
+        public bool SetDestinationToPosition(Vector3 position, bool checkForPath = false)
+        {
+            if (checkForPath)
+            {
+                position = RoundManager.Instance.GetNavMeshPosition(position, RoundManager.Instance.navHit, 1.75f);
+                path1 = new NavMeshPath();
+                if (!agent.CalculatePath(position, path1))
+                {
+                    return false;
+                }
+                if (Vector3.Distance(path1.corners[path1.corners.Length - 1], RoundManager.Instance.GetNavMeshPosition(position, RoundManager.Instance.navHit, 2.7f)) > 1.55f)
+                {
+                    return false;
+                }
+            }
+            moveTowardsDestination = true;
+            destination = RoundManager.Instance.GetNavMeshPosition(position, RoundManager.Instance.navHit, -1f);
+            return true;
+        }
+
+        public void Disappear()
+        {
+
+        }
+
+        [ClientRpc]
+        public void DisappearClientRpc()
+        {
+
+        }
     }
 }
